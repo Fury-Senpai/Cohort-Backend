b85a519ef875b85129d7697001797c730d95bdbd
Fury-Senpai <harbola03nitish@gmail.com>
2026-01-24 15:11:43 +0530

    wrote app.listen for starting server on port 3000

--- day-1/app.js
+++ day-1/app.js
@@ -1,3 +1,5 @@
 const express = require('express')
 
-express()
+const app = express() // created a server instance and saved it into a const app
+
+app.listen(3000) // app.listen will start server on port 3000
