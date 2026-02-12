0cbedbd1ad7407b529fa7022904947712a6531ad
Fury-Senpai <harbola03nitish@gmail.com>
2026-02-12 13:11:35 +0530

    Created server and wrote get , post and del API for the note task

--- day-4/src/app.js
+++ day-4/src/app.js
@@ -1,2 +1,43 @@
 // - server create karna
 // - server ko config karna
+
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+const notes = [];
+
+// 
+// GET ROUTES
+// 
+
+app.get('/' , (req,res)=>{
+    res.send('Welcome');
+})
+
+app.get('/show' , (req,res)=>{
+    res.send(notes)
+    
+})
+
+// 
+// POST ROUTES
+// 
+app.post('/create-note' , (req,res)=>{
+    notes.push(req.body)
+    res.send(req.body);
+})
+
+// 
+// DELETE ROUTE
+// 
+
+app.delete('/delete-note/:id' , (req,res)=>{
+    const {id} = req.params;
+    notes.splice(id , 1);
+    res.send('Deleted')
+})
+
+
+module.exports = app;
