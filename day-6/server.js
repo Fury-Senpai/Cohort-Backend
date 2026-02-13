c01b4865966c66a692be2e68b46217942b6c28f2
Fury-Senpai <harbola03nitish@gmail.com>
2026-02-13 12:48:59 +0530

    connected database to the sever

--- day-6/server.js
+++ day-6/server.js
@@ -1,7 +1,14 @@
 // - Server run karna
 // - Server ko database se connect karna
 const app = require('./src/app');
+const mongoose = require('mongoose')
 const PORT = 3000;
+
+async function connectToDb(){
+    await mongoose.connect("mongodb+srv://harbola03nitish_db_user:J91I6Lk8UW3MfJ83@cluster0.6xajgkw.mongodb.net/day-6");
+    console.log("Database connected");
+}
+connectToDb()
 
 app.listen(PORT , ()=>{
     console.log(`Server is running on Port ${PORT}`);
--- day-6/src/app.js
+++ day-6/src/app.js
@@ -4,4 +4,11 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+app.get('/' , (req,res)=>{
+    res.status(200).json(
+        {
+            message : "working"
+        }
+    )
+})
 module.exports = app;
