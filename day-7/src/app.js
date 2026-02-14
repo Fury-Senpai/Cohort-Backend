562b1f182219b4739aecf87076c1dd02a68eed3d
Fury-Senpai <harbola03nitish@gmail.com>
2026-02-14 23:19:12 +0530

    updated app.js , database.js and notes.model.js

--- day-7/src/app.js
+++ day-7/src/app.js
@@ -1,7 +1,33 @@
 // - server ko create karna
 // - server ko config karna
 const express = require('express');
+const noteModel = require('./models/notes.model')
 const app = express();
 app.use(express.json());
 
+// POST /note
+app.post('/note', async (req,res)=>{
+    const {title , desc} = req.body;
+    const note = await noteModel.create(
+        {
+            title : title,
+            desc : desc
+        }
+    )
+    res.status(201).json(
+        {
+            message : "note created",
+            note
+        }
+    )
+
+})
+
+// GET /note
+app.get('/note' , async (req,res)=>{
+    const notes = await noteModel.find();
+    res.status(200).json(
+        {notes}
+    )
+})
 module.exports = app;
--- day-7/src/config/database.js
+++ day-7/src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 async function connectToDb(){
     try {
-        await mongoose.connect("mongodb+srv://harbola03nitish_db_user:J91I6Lk8UW3MfJ83@cluster0.6xajgkw.mongodb.net/day-7");
+        await mongoose.connect(process.env.MONGO_DB_URI);
         console.log("Connected to Database");
     } catch (error) {
         console.log(error.message);
